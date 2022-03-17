b55cf5efc4c8e451e44c57dd9ab2d87a50ce271e
Puja0208 <pooja2081997@gmail.com>
2022-03-17 07:02:34 +0530

    Initial commit

---
+++ src/store/store.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+
+let globalState = {};
+let listeners = []; //to listen to changes in state
+let actions = {};
+
+export const useStore = () => {
+  const setState = useState(globalState)[1];
+
+  const dispatch = (actionIdentifier) => {
+    const newState = actions[actionIdentifier](globalState);
+    globalState = { ...globalState, newState };
+
+    //inform all listeners about this state update
+    for (const listener of listeners) {
+      listener(globalState);
+    }
+  };
+
+  useEffect(() => {
+    listeners.push(setState);
+
+    return () => {
+      listeners = listeners.filter((li) => li !== setState);
+    };
+  }, [setState]);
+
+  return [globalState, dispatch];
+};
+
+export const initStore = (userActions, initialState) => {
+  if (initialState) {
+    globalState = { ...globalState, ...initialState };
+  }
+  actions = { ...actions, ...userActions };
+};
